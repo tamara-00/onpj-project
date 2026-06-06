5063b3b222c7b64ab14588957431bffc864d8f4b
Tamara Stojanoska <tamarastojanoska@Mac.home>
2026-06-06 22:14:48 +0200

    Changes

--- fix_notebooks.py
+++ fix_notebooks.py
@@ -3,21 +3,14 @@ import glob
 import sys
 import os
 
-folder = sys.argv[1] if len(sys.argv) > 1 else '.'
-notebooks = glob.glob(os.path.join(folder, '**', '*.ipynb'), recursive=True)
+folder = sys.argv[1] if len(sys.argv) > 1 else "."
 
-if not notebooks:
-    print("No .ipynb files found.")
-else:
-    for path in notebooks:
-        with open(path) as f:
-            nb = json.load(f)
-        if 'widgets' in nb.get('metadata', {}):
-            del nb['metadata']['widgets']
-            with open(path, 'w') as f:
-                json.dump(nb, f, indent=2)
-            print(f"Fixed: {path}")
-        else:
-            print(f"Skipped (no widgets): {path}")
+for path in glob.glob(os.path.join(folder, "**", "*.ipynb"), recursive=True):
+    with open(path) as f:
+        nb = json.load(f)
 
-print("\nDone!")
+    if "widgets" in nb.get("metadata", {}):
+        del nb["metadata"]["widgets"]
+
+        with open(path, "w") as f:
+            json.dump(nb, f, indent=2)
